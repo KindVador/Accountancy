77832f376dbd17a505cc8807881b1fee96def451
Florian Contival <11033778+KindVador@users.noreply.github.com>
2021-10-21 22:02:37 +0200

    model

--- src/model.cpp
+++ src/model.cpp
@@ -1,6 +1,10 @@
 #include "model.hpp"
 
 Model::Model(): _ownerModel(new OwnerModel), _currencyModel(new CurrencyModel) {
+    Currency euro = Currency();
+    euro.setName("Euro");
+    euro.setSymbol("€");
+    _ownerModel->addOwner("Florian", euro, 0, "", false);
 }
 
 Model::~Model() {
