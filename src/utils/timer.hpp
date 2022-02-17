ac17bb78e45a2eb789afd61b2ce3359dbb760a45
Florian Contival <kindvador@gmail.com>
2022-02-17 23:46:12 +0100

    add Timer class for benchmarking if necessary

---
+++ src/utils/timer.hpp
@@ -0,0 +1,38 @@
+#ifndef ACCOUNTANCY_TIMER_HPP
+#define ACCOUNTANCY_TIMER_HPP
+
+#include <chrono>
+#include <iostream>
+
+class Timer
+{
+public:
+    Timer()
+    {
+        m_StartTimePoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        Stop();
+    }
+
+    void Stop()
+    {
+        auto endTimePoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimePoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
+
+        auto duration = end - start;
+        double ms = duration * 0.001;
+
+        std::cout << duration << "us (" << ms << "ms \n";
+    }
+
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimePoint;
+};
+
+#endif //ACCOUNTANCY_TIMER_HPP
