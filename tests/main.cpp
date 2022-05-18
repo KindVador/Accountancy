73be1a9985b1d583b461ad7182d917731d18340a
Florian Contival <kindvador@gmail.com>
2022-05-18 10:06:44 +0200

    provide our own main for catch2

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,13 @@
 // Demonstrate how to create a Catch main() for testing Qt GUI code
 
 #define CATCH_CONFIG_RUNNER
-#include <Catch.hpp>
+#include <catch2/catch.hpp>
 
 #include <QApplication>
 
 int main (int argc, char* argv[])
 {
-    QApplication app(argc, argv);                   // -platform offscreen
-    int result = Catch::Session().run(argc, argv);      // --break
+    QApplication app(argc, argv);               // We can pass as argument "-platform offscreen" if needed
+    int result = Catch::Session().run(argc, argv); // We can pass as argument "--break" if needed
     return result;
 }
